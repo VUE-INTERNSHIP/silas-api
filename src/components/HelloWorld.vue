670d1c5bc180c1143359c3373878122e86a83ed2
Silas Bage <silasbage2018@gmail.com>
2022-09-18 23:43:55 +0100

    silas-api

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+      <h1>Employee List</h1>  
+      <div v-for="item in list" v-bind:key="item.id">
+        <p>Brand:{{item.brand}}</p>
+        <p>Category:{{item.category}}</p>
+        <p>Discount:{{item.discountPercentage}}</p>
+        <img :src="item.thumbnail"/>
+        <p>Price:{{item.price}}</p>
+        <p>Rating:{{item.rating}}</p>
+      </div>
+  </div>
+</template>
+<script>
+import Vue from 'vue';
+  import axios from 'axios'
+import VueAxios from 'vue-axios'
+Vue.use(VueAxios,axios)
+export default {
+  name: 'App',
+  data()
+  {
+      return{list:undefined}
+  },
+  mounted()
+  {
+      Vue.axios.get('https://dummyjson.com/products')
+      .then((resp)=>{
+          this.list=resp.data.products;
+          console.warn(resp.data.products)
+      })
+  }
+}
+</script>
